5e1542bf5cd7e4ff398761df80582ebd9dd91757
Zuban Yaroslav <yaroslav.zuban7029@gmail.com>
2021-04-12 15:15:56 +0700

    Initial commit

---
+++ Zadanie1.1.py
@@ -0,0 +1,9 @@
+#import ctypes
+
+#primer1 = ctypes.CDLL('C:|Users|Zuban Yaroslav|source|repos|PythonLaba1_5|PythonLaba1_5|PythonLaba.so')
+
+#primer1.primer.restype = ctypes.c_int
+#primer1.primer.argtypes = [ctypes.c_int]
+
+print("Hello")
+# print (ctypes.CDLL('PythonLaba.so').primer(4))
---
+++ primer.py
@@ -0,0 +1,15 @@
+from ctypes import *
+cdll.msvcrt("./PythonLaba1_5/PythonLaba1_5/test.so")
+
+
+
+a = 2
+resultat = a + 2
+print(resultat)
+
+test=ctypes.CDLL('./PythonLaba1_5/PythonLaba1_5/test.so')
+
+test.primer_int.restype=ctypes.c_int
+test.primer_int.argtypes = [ctypes.c_int]
+print("решение ", test.primer_int)
+
